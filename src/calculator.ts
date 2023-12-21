c4d0b6ef8a3169123bfb6c93370942eceecacf0f
yuu-chien <sarsh40320@gmail.com>
2023-12-21 16:26:22 +0800

    fix: add the return type to the function

--- src/calculator.ts
+++ src/calculator.ts
@@ -33,7 +33,7 @@ export function multiply(a: number, b: number): number {
  * @param a - 第一個數字
  * @param b - 第二個數字
  */
-export function divide(a: number, b: number) {
+export function divide(a: number, b: number): number {
     // 在此實現函式
     if (b === 0) {
         throw new Error('Cannot divide by zero')

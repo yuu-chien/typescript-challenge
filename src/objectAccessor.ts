155b8010da3226d0a5ecfa5055ee24368b03d441
Amy <sarsh40320@gmail.com>
2024-01-05 15:48:31 +0800

    feat: practices of Generics

--- src/objectAccessor.ts
+++ src/objectAccessor.ts
@@ -13,5 +13,9 @@
  * @returns - 回傳一個物件，該物件有 get 和 set 兩個方法
  */
 export function createObjectAccessor<T>(obj: T) {
-    // 請在此處寫下你的程式碼
+
+    // written by ChatGPT
+    const get = (a: keyof T) => obj[a]
+    const set = (b: keyof T, val: T[keyof T]) => obj[b] = val
+    return { get, set }
 }
